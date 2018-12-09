1588668a87dcfd23bd2906e2620026e9ea88d675
Eivind Fonn <evfonn@gmail.com>
2018-12-09 14:28:52 +0100

    Transforms: Compute gradient information

--- src/Transforms.jl
+++ src/Transforms.jl
@@ -15,10 +15,23 @@ struct Updim{M,N,R} <: Transform{M,R}
 end
 
 function codegen(::Type{<:Updim{M,N}}, trf, point) where {M,N}
-    exprs = [:($point[$(i)] = $point[$(i-1)]) for i in M:-1:N+1]
+    exprs = [:($point[$i] = $point[$(i-1)]) for i in M:-1:N+1]
     quote
         $(exprs...)
         $point[$N] = $trf.data
+    end
+end
+
+codegen(tp::Type{<:Updim{1,1}}, trf, point, grad) = codegen(tp, trf, point)
+
+function codegen(tp::Type{<:Updim{2,N}}, trf, point, grad) where {N}
+    exprs = [:($grad[$i,:] .= $grad[$(i-1),:]) for i in 2:-1:N+1]
+    quote
+        $(codegen(tp, trf, point))
+        $(exprs...)
+        $grad[$N, :] .= 0.0
+        $grad[1,2] = $grad[2,1]
+        $grad[2,2] = -$grad[1,1]
     end
 end
 
@@ -31,9 +44,15 @@ function codegen(::Type{<:Shift}, trf, point)
     :($point .+= trf.data)
 end
 
+codegen(tp::Type{<:Shift}, trf, point, grad) = codegen(tp, trf, point)
+
 
 @generated function apply!(point::MVector{M,R}, trf::Transform{M,R}) where {M,R}
     codegen(trf, :trf, :point)
+end
+
+@generated function apply!(point::MVector{M,R}, grad::MMatrix{M,M,R}, trf::Transform{M,R}) where {M,R}
+    codegen(trf, :trf, :point, :grad)
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra
 using Random
 using StaticArrays
 using Test
@@ -26,6 +27,23 @@ using Fatale.Transforms
     apply!(res, trf)
     @test res == [initial[1], initial[2], trf.data]
 
+    # Dimension 1 -> 2
+    initial = rand(Float64, 2)
+
+    trf = Updim{2,1,Float64}(rand(Float64))
+    res = MVector{2}(initial)
+    grad = MMatrix{2,2,Float64}(I)
+    apply!(res, grad, trf)
+    @test res == [trf.data, initial[1]]
+    @test grad == [0.0 1.0; 1.0 0.0]
+
+    trf = Updim{2,2,Float64}(rand(Float64))
+    res = MVector{2}(initial)
+    grad = MMatrix{2,2,Float64}(I)
+    apply!(res, grad, trf)
+    @test res == [initial[1], trf.data]
+    @test grad == [1.0 0.0; 0.0 -1.0]
+
     # Dimension 0 -> 1
     trf = Updim{1,1,Float64}(rand(Float64))
     res = MVector{1,Float64}(undef)
@@ -39,8 +57,14 @@ end
 
     trf = Shift(SVector{4}(rand(Float64, 4)))
     initial = rand(Float64, 4)
-    res = MVector{4}(initial)
 
+    res = MVector{4}(initial)
     apply!(res, trf)
     @test res == initial + trf.data
+
+    res = MVector{4}(initial)
+    grad = MMatrix{4,4,Float64}(I)
+    apply!(res, grad, trf)
+    @test res == initial + trf.data
+    @test grad == I
 end
