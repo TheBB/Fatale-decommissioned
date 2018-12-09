3e4d851af67ae007235fc63086affd46de08efea
Eivind Fonn <evfonn@gmail.com>
2018-12-09 10:26:54 +0100

    Shift transform

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,12 +5,12 @@ using Test
 using Fatale.Transforms
 
 
-@testset "DimensionTransform" begin
+@testset "Updim" begin
     Random.seed!(201812081344)
 
     # Dimension 2 -> 4
     data = SVector{4}(rand(Float64, 4))
-    trf = DimensionTransform(data, SVector(1,4))
+    trf = Updim(data, SVector(1,4))
     res = MVector{4,Float64}(undef)
 
     apply!(res, trf, SVector(1.0, 3.0))
@@ -18,7 +18,7 @@ using Fatale.Transforms
 
     # Dimension 0 -> 2
     data = SVector{2}(rand(Float64, 2))
-    trf = DimensionTransform(data, SVector{0,Int}())
+    trf = Updim(data, SVector{0,Int}())
     res = MVector{2,Float64}(undef)
 
     apply!(res, trf, SVector{0,Float64}())
@@ -26,13 +26,25 @@ using Fatale.Transforms
 
     # Dimension 2 -> 2
     data = SVector{2}(rand(Float64, 2))
-    trf = DimensionTransform(data, SVector(1,2))
+    trf = Updim(data, SVector(1,2))
     res = MVector{2,Float64}(undef)
 
     apply!(res, trf, SVector(1.0, 3.0))
     @test res == [1.0, 3.0]
 
     # Bounds check
-    @test_throws BoundsError DimensionTransform(SVector(1.0, 2.0), SVector(1, 3))
-    @test_throws DimensionMismatch DimensionTransform(SVector(1.0, 2.0), SVector(1, 1, 1))
+    @test_throws BoundsError Updim(SVector(1.0, 2.0), SVector(1, 3))
+    @test_throws DimensionMismatch Updim(SVector(1.0, 2.0), SVector(1, 1, 1))
+end
+
+
+@testset "Shift" begin
+    Random.seed!(201812091010)
+
+    trf = Shift(SVector{4}(rand(Float64, 4)))
+    root = rand(Float64, 4)
+    res = MVector{4}(root)
+
+    apply!(res, trf, res)
+    @test res == root + trf.data
 end
