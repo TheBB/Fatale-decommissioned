755626f604e8b9ad01ad7fb242e025bd862a1bfc
Eivind Fonn <evfonn@gmail.com>
2018-12-09 10:59:22 +0100

    Transforms: Use generated functions

--- src/Transforms.jl
+++ src/Transforms.jl
@@ -2,26 +2,38 @@ module Transforms
 
 using StaticArrays
 
+export Transform
 export Updim, Shift
 export apply!
 
 
-struct Updim{M,N,R<:Real}
+abstract type Transform{M,R<:Real} end
+
+
+struct Updim{M,N,R} <: Transform{M,R}
     data :: R
 end
 
-function apply!(point::MVector{M,R}, trf::Updim{M,N,R}) where {M,N,R<:Real}
-    point[N+1:M] = point[N:M-1]
-    point[N] = trf.data
+function codegen(::Type{Updim{M,N,R}}, trf, point) where {M,N,R}
+    exprs = [:($point[$(i)] = $point[$(i-1)]) for i in M:-1:N+1]
+    quote
+        $(exprs...)
+        $point[$N] = $trf.data
+    end
 end
 
 
-struct Shift{M,R<:Real}
+struct Shift{M,R<:Real} <: Transform{M,R}
     data :: SVector{M,R}
 end
 
-function apply!(point::MVector{M,R}, trf::Shift{M,R}) where {M,R<:Real}
-    point .+= trf.data
+function codegen(::Type{Shift{M,R}}, trf, point) where {M,R}
+    :($point .+= trf.data)
+end
+
+
+@generated function apply!(point::MVector{M,R}, trf::Transform{M,R}) where {M,R}
+    codegen(trf, :trf, :point)
 end
 
 end
