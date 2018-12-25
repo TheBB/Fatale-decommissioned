880a36826b35545d948724a99f6a4becf63d6966
Eivind Fonn <evfonn@gmail.com>
2018-12-25 14:32:33 +0100

    Add monomials test

--- test/Evaluables.jl
+++ test/Evaluables.jl
@@ -1,4 +1,4 @@
-@testset "LocalPoints" begin
+@testset "LocalPoint" begin
     Random.seed!(201812131215)
     func = compile(localpoint(2))
 
@@ -16,7 +16,7 @@
 end
 
 
-@testset "GlobalCoords" begin
+@testset "GlobalPoint" begin
     Random.seed!(201812131219)
     func = compile(globalpoint(2))
     shift = @SVector rand(2)
@@ -32,4 +32,17 @@ end
     sub = SubElement(Updim{2,2}(4.0), element)
     quadpt = @SVector rand(1)
     @test func(sub, quadpt) ≈ [quadpt[1], 4.0] + shift
+end
+
+
+@testset "Monomials" begin
+    func = compile(Monomials(localpoint(3), 4))
+    element = FullElement(Empty{3,Float64}())
+    quadpt = @SVector [1.0, 2.0, 3.0]
+
+    @test func(element, quadpt) ≈ [
+        1.0 1.0 1.0  1.0  1.0;
+        1.0 2.0 4.0  8.0 16.0;
+        1.0 3.0 9.0 27.0 81.0;
+    ]
 end
