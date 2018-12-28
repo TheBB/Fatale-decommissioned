0ddc1ed2f1b32ca99b11e6da68d35dd1f0c0e0ce
Eivind Fonn <evfonn@gmail.com>
2018-12-28 14:31:47 +0100

    Lagrangian one-dimensional shape functions

--- src/Domains.jl
+++ src/Domains.jl
@@ -1,20 +1,26 @@
 module Domains
 
+using LinearAlgebra
 using StaticArrays
 using ..Transforms
 using ..Elements
+using ..Evaluables
 
 export TensorDomain
+export basis, Lagrange
 
 
 abstract type Domain{Elt,Ref,N} <: AbstractArray{Elt,N} end
 
+# Basis types
+abstract type Basis end
+struct Lagrange <: Basis end
+
 
 struct TensorDomain{D} <: Domain{FullElement{D,Shift{D,Float64}}, Tensor{D,NTuple{D,Simplex{1}}}, D}
     size :: NTuple{D,Int}
+    TensorDomain(I::Int...) = new{length(I)}(I)
 end
-
-TensorDomain(I::Int...) = TensorDomain{length(I)}(I)
 
 @inline Base.size(self::TensorDomain) = self.size
 @inline Base.IndexStyle(::Type{TensorDomain}) = IndexCartesian()
@@ -22,6 +28,16 @@ TensorDomain(I::Int...) = TensorDomain{length(I)}(I)
     @boundscheck checkbounds(self, I...)
     shift = SVector{D,Float64}(I) - 1.0
     FullElement(Shift(shift))
+end
+
+function basis(self::TensorDomain{D}, ::Type{Lagrange}, degree) where {D}
+    # Generate N single-dimensional Lagrangian bases of the right degree
+    poly = Monomials(localpoint(D), degree)
+    coeffs = inv(range(0, 1, length=degree+1) .^ reshape(0:degree, 1, :))
+    coeffs = MMatrix{degree+1, degree+1}(coeffs)
+    basis1d = Contract(poly, Constant(coeffs), (1, 2), (2, 3), (1, 3))
+
+    basis1d
 end
 
 end
--- src/Fatale.jl
+++ src/Fatale.jl
@@ -5,7 +5,7 @@ using StaticArrays
 include("Utils.jl")
 include("Transforms.jl")
 include("Elements.jl")
-include("Domains.jl")
 include("Evaluables.jl")
+include("Domains.jl")
 
 end # module
