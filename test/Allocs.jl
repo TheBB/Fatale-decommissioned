49ed72919fc074c1bbcd162312a48ab7a4e2479f
Eivind Fonn <evfonn@gmail.com>
2019-01-10 13:59:02 +0100

    Faster allocation tests

--- test/Allocs.jl
+++ test/Allocs.jl
@@ -6,12 +6,16 @@ macro noallocs(expr)
     end
 end
 
+macro bench(expr)
+    :(@benchmark $expr samples=1 evals=1)
+end
+
 
 function _trfchain()
     trf = Chain(Updim{1,2}(1.0), Shift(SVector(4.0, 5.0)))
     initial = @SVector rand(1)
     igrad = SMatrix{1,1,Float64}(I)
-    @benchmark $trf($initial, $igrad)
+    @bench $trf($initial, $igrad)
 end
 
 @testset "Transform chain" begin
@@ -23,7 +27,7 @@ function _globalpoint()
     func = compile(globalpoint(2))
     element = FullElement(Shift(@SVector rand(2)))
     quadpt = @SVector rand(2)
-    @benchmark $func($element, $quadpt)
+    @bench $func($element, $quadpt)
 end
 
 @testset "GlobalPoint" begin
@@ -35,7 +39,7 @@ function _monomials()
     func = compile(Monomials(localpoint(3), 4))
     element = FullElement(Empty{3,Float64}())
     quadpt = @SVector [1.0, 2.0, 3.0]
-    @benchmark $func($element, $quadpt)
+    @bench $func($element, $quadpt)
 end
 
 @testset "Monomials" begin
