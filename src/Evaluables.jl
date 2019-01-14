cd70c30f18ec161f3a26715b9052518f2e488cfd
Eivind Fonn <evfonn@gmail.com>
2019-01-14 17:16:47 +0100

    Evaluation tree inspection

--- src/Evaluables.jl
+++ src/Evaluables.jl
@@ -2,7 +2,7 @@ module Evaluables
 
 import Base: @_inline_meta, Broadcast
 import Base.Broadcast: broadcast_shape
-import Base.Iterators: flatten, product
+import Base.Iterators: flatten, isdone, product, repeated, Stateful
 
 using DataStructures
 using LinearAlgebra
@@ -14,7 +14,7 @@ using ..Elements
 
 export Constant, Contract, Elementwise, Inflate, Monomials
 export localpoint, localgrad, globalpoint, globalgrad
-export compile, optimize, indices
+export ast, compile, optimize, indices
 
 
 """
@@ -38,6 +38,36 @@ Base.:(==)(l::Evaluable, r::Evaluable) = typeof(l) == typeof(r) && arguments(l) 
 # Code generation for most evaluables involves simply calling the
 # evaluable object directly.
 codegen(::Type{<:Evaluable}, self, args...) = :($self($(args...)))
+
+
+"""
+    ast(ev::Evaluable)
+
+Returns a string with a human-readable representation of evaluation
+tree of `ev`.
+"""
+function ast(self::Evaluable)
+    sequence = linearize(self)
+    _ast!(Set{Int}(), length(sequence), sequence)
+end
+
+function _ast!(seen, index, sequence)
+    index in seen && return string("%", index)
+
+    s = ""
+    arginds = Stateful(sequence[index].arginds)
+    for argind in arginds
+        bridge = isdone(arginds) ?
+            flatten((("└─",), repeated("  "))) :
+            flatten((("├─",), repeated("│ ")))
+        sublines = split(_ast!(seen, argind, sequence), "\n")
+        subtree = join((string(b, s) for (b, s) in zip(bridge, sublines)), "\n")
+        s = string(s, "\n", subtree)
+    end
+
+    push!(seen, index)
+    string("%", index, " = ", repr(sequence[index].func), s)
+end
 
 
 include("evaluables/definitions.jl")
